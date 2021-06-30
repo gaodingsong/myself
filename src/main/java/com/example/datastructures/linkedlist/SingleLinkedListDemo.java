a534a92231bc9dadaf8b0fcac90cb66765025de3
gaodingsong <gds941104>
2021-06-30 09:48:45 +0800

    增加面试题查找单链表中的倒数第k个节点和求单链表的有效节点的个数

--- src/main/java/com/example/datastructures/linkedlist/SingleLinkedListDemo.java
+++ src/main/java/com/example/datastructures/linkedlist/SingleLinkedListDemo.java
@@ -35,14 +35,68 @@ public class SingleLinkedListDemo {
 
         // 删除节点
         singleLinkedList.del(1);
-        singleLinkedList.del(2);
-        singleLinkedList.del(3);
+
         singleLinkedList.del(4);
         // 删除后的
         System.out.println("删除后的节点列表展示~~~~~~~~~~");
         singleLinkedList.list();
 
+        //测试一下，求单链表的有效节点的个数
+        System.out.println("有效的节点个数：" + getLength(singleLinkedList.getHead()));
+
+
+        //测试  查找单链表中的倒数第k个节点
+        HeroNode res = findLastIndexNode(singleLinkedList.getHead(), 2);
+
+        System.out.println("倒数第一个" + res);
+
+
     }
+
+    // 查找单链表中的倒数第k个节点
+
+
+    public static HeroNode findLastIndexNode(HeroNode head,int index){
+
+        //判断如果为null就返回null
+        if (head.next == null){
+            return null;
+        }
+        // 第一个遍历得到链表的长度（节点个数）
+        int size = getLength(head);
+        // 第二次遍历 size-index位置，就是我们倒数第k个节点
+        // 先做一个index的校验
+        if (index <= 0 || index >size){
+            return null;
+        }
+        // 定义辅助变量你 for 循环定位到倒数的index
+        HeroNode cur = head.next;
+        for (int i = 0; i < size -index; i++) {
+            cur = cur.next;
+
+        }
+        return cur;
+
+    }
+
+
+    // 获取单链表的有效节点的个数
+    public static int getLength(HeroNode head){
+        if (head.next == null){
+            return 0;
+        }
+        int length = 0;
+        // 定义一个辅助变量,这里没有统计头节点
+        HeroNode cur = head.next;
+        while (cur != null){
+            length++;
+            cur = cur.next;
+        }
+        return length;
+
+    }
+
+
 
 }
 
@@ -50,6 +104,13 @@ public class SingleLinkedListDemo {
 class SingleLinkedList{
     // 先初始化一个头节点，头节点不要动，不存放具体的数据
     private HeroNode head = new HeroNode(0,"","");
+
+    // 返回头节点
+    public HeroNode getHead(){
+        return head;
+    }
+
+
     // 添加节点到单项链表
     // 思路：当不考虑编号的顺序时
     // 1.先张公案到当前链表的最后节点
