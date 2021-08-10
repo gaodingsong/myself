7a20925f3b47e69eafeeeb9a1436c1f5bdafbc2b
gaodingsong <903999764@qq.com>
2021-08-10 17:30:38 +0800

    添加工具类

--- src/main/java/com/example/util/SearchUtils.java
+++ src/main/java/com/example/util/SearchUtils.java
@@ -8,6 +8,35 @@ import java.util.ArrayList;
 public class SearchUtils {
 
 
+    /**
+     * 插值查找算法
+     * @param arr 原始数组
+     * @param left 左边索引
+     * @param right 右边索引
+     * @param findVal 要查找的值  如果找到就返回对应的下标 否则就返回-1
+     * @return
+     */
+    public static int insertValueSearch(int[] arr,int left ,int right,int findVal){
+
+        System.out.println("查找次数~~");
+        // 因为数组是有序的，要是小于最小的或者大于最大的 就肯定找不到
+        //  findVal < arr[0]  || findVal >arr[arr.length-1] 这两个必须要  否则mid可能越界
+        if (left > right || findVal < arr[0]  || findVal >arr[arr.length-1]  ){
+            return -1;
+        }
+        int mid = left + (right - left) * (findVal - arr[left]) / (arr[right] - arr[left]);
+        int midVal= arr[mid];
+        if (findVal > midVal ){
+            // 向右递归
+            return insertValueSearch(arr,mid+1,right,findVal);
+        }else if (findVal < midVal){
+            // 向左递归
+            return insertValueSearch(arr,left,mid-1,findVal);
+        }else {
+            return mid;
+        }
+
+    }
 
 
 
